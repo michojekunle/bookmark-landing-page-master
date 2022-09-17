e6f8dce4b27af0081e57574006a1cf8e132e1578
AMD <michojekunle1@gmail.com>
2022-09-17 07:41:45 +0100

    made changes

--- main.js
+++ main.js
@@ -20,18 +20,21 @@ featuresNavList.forEach((feature, index) => {
 
         if(index === 0){
             featureIllustration.src = './images/illustration-features-tab-1.svg';
+            // featureIllustration.style.minWidth = '32rem';
             featureTitle.innerText = 'Bookmark in one click';
             featureAbout.innerText = ` Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.`;
         }
 
         if(index === 1){
-            featureIllustration.src = './images/illustration-features-tab-2.svg'
+            featureIllustration.src = './images/illustration-features-tab-2.svg';
+            // featureIllustration.style.minWidth = '32rem';
             featureTitle.innerText = 'Intelligent search';
             featureAbout.innerText = `Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.`;
         } 
 
         if(index === 2){
             featureIllustration.src = './images/illustration-features-tab-3.svg';
+            // featureIllustration.style.minWidth = '32rem';
             featureTitle.innerText = 'Share your bookmarks';
             featureAbout.innerText = `Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.`;
         }
